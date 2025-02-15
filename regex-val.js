9a30d2c483a2599299ae64f64b31c8fef4558793
AliceUwase <a.uwase3@alustudent.com>
2025-02-15 16:16:53 +0200

    Added pattern to validate the URLs

--- regex-val.js
+++ regex-val.js
@@ -3,25 +3,4 @@ const patterns = {
     
     url: /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/,
     
-    phone: /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/,
     
-    creditCard: /^(\d{4}[-\s]?){3}\d{4}$/,
-    
-    time: /^(?:(?:([01]?\d|2[0-3]):([0-5]\d))|((?:1[0-2]|0?[1-9]):[0-5]\d\s?(?:AM|PM)))$/i
-};
-
-function extractData(text, type) {
-    if (!patterns[type]) {
-        throw new Error(`Pattern type "${type}" not supported`);
-    }
-    
-    return patterns[type].test(text);
-}
-
-
-console.log(extractData("user@example.com", "email")); 
-console.log(extractData("https://www.example.com", "url")); 
-console.log(extractData("(123) 456-7890", "phone")); 
-console.log(extractData("1234 5678 9012 3456", "creditCard")); 
-console.log(extractData("14:30", "time")); 
-console.log(extractData("2:30 PM", "time"));
